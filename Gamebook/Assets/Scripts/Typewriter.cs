f9bd04d8eede5c068f2c21dea747c1645f6cb210
DESKTOP-LGG22I5\유용원 <qlxc456@gmail.com>
2023-12-28 08:58:35 +0900

    fixed untracked files

---
+++ Gamebook/Assets/Scripts/Typewriter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Febucci.UI.Core;
+using TMPro;
+public abstract class TypewriterCore : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
